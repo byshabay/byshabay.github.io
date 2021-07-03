282b43444d1aabd1e6499e0537bdd599c2ec6199
Alyona Shabaykina <shabbaykina@mail.ru>
2021-07-03 11:48:04 +0300

    test

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName(){
+    var myName = prompt('Введите свое имя.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = "Привет, " + myName;
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = "Привет, " + storedName;
+}
+
+myButton.onclick = function(){
+    setUserName();
+}
+
+const para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
+function updateName(){
+    let name = prompt('Введите абзац');
+    para.textContent = 'Абзац' + name;
+}
